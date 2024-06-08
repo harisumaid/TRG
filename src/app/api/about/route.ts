3b6026a77b33a5c3a9a6c4f52ab3ca31381d3c28
harisumaid <harisumaid27@gmail.com>
2024-06-08 16:09:52 +0530

    fix: add error handling on backend api routes

--- src/app/api/about/route.ts
+++ src/app/api/about/route.ts
@@ -2,15 +2,20 @@ import config from "./../../../config/config";
 const { cmsReadApiToken, cmsServerEndpoint } = config;
 
 export async function GET(request: Request){
-    const res = await fetch(`${cmsServerEndpoint}/api/about`,{
-        headers:{
-            "Authorization": `Bearer ${cmsReadApiToken}`
-        }
-    }).catch(err=>{
-        console.log(err);
-        return err;
-    });
-
-    const data = await res.json();
-    return Response.json({ data: data.data });
+    try {
+        const res = await fetch(`${cmsServerEndpoint}/api/about`,{
+            headers:{
+                "Authorization": `Bearer ${cmsReadApiToken}`
+            }
+        }).catch(err=>{
+            console.log(err);
+            return err;
+        });
+    
+        const data = await res.json();
+        return Response.json({ data: data.data });
+    } catch (error) {
+        console.log(error);
+        return Response.json({ data: {} });
+    }
 }
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,12 +11,12 @@ export default async function About() {
     <RevealOnScroll>
       <div id="AboutUs" className="h-screen w-full snap-center snap-always">
         <div className="w-full absolute text-sm sm:text-xl lg:text-3xl text-center font-extralight ">
-          {aboutUs.attributes.aboutHeading}
+          {aboutUs?.attributes?.aboutHeading || "DUMMY ABOUT HEADING"}
         </div>
         <div className="h-full w-full flex justify-center sm:space-x-4 lg:space-x-12 sm:px-8 items-center flex-col sm:flex-row space-y-7 sm:space-y-0 px-8">
           <Image
             className="w-36 sm:w-52 h-36 sm:h-52 rounded-lg transition hover:scale-110 hover:shadow-lg ease-in-out duration-500"
-            src={aboutUs.attributes.aboutImage}
+            src={aboutUs?.attributes?.aboutImage || "/DUMMY_IMAGE_URL"}
             alt={"About us image"}
             width={500}
             height={500}
@@ -24,7 +24,7 @@ export default async function About() {
           <div
             className="w-4/5 sm:w-3/5 lg:w-2/5 text-xs sm:text-sm lg:text-lg text-center"
             dangerouslySetInnerHTML={{
-              __html: aboutUs.attributes.aboutDetails,
+              __html: aboutUs?.attributes?.aboutDetails || "DUMMY ABOUT DETAILS",
             }}
           ></div>
         </div>
