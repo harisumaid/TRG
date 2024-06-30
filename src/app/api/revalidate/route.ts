aa3cfc4f0be37e023aa0b86f75322305afca8b5f
harisumaid <harisumaid27@gmail.com>
2024-06-30 13:02:24 +0530

    feat: add route to invalidate cached cms data

---
+++ src/app/api/revalidate/route.ts
@@ -0,0 +1,22 @@
+import { revalidatePath } from "next/cache";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const path = request.nextUrl.searchParams.get("path");
+
+  if (path) {
+    revalidatePath(path);
+    return Response.json({ revalidated: true, now: Date.now() });
+  }
+
+  return Response.json(
+    {
+      revalidated: false,
+      now: Date.now(),
+      message: "Path not specified",
+    },
+    {
+      status: 400,
+    }
+  );
+}
