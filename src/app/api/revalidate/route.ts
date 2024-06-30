eed81d522c90b195b9e0362056f1d6f5db935599
harisumaid <harisumaid27@gmail.com>
2024-06-30 14:46:52 +0530

    fix: add POST route to cache invalidate api as strapi webhook sends POST request

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -20,3 +20,23 @@ export async function GET(request: NextRequest) {
     }
   );
 }
+
+export async function POST(request: NextRequest) {
+    const path = request.nextUrl.searchParams.get("path");
+  
+    if (path) {
+      revalidatePath(path, "layout");
+      return Response.json({ revalidated: true, now: Date.now() });
+    }
+  
+    return Response.json(
+      {
+        revalidated: false,
+        now: Date.now(),
+        message: "Path not specified",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
